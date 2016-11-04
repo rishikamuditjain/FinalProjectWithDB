9dca592920152bb0cfc2e55e1189d3a7bf7f3a92
rishikamaroo <rishikamaroo@gmail.com>
2016-11-04 18:16:38 +0000

    UW Project

---
+++ FinalProject-UW/ConsoleApplication1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Stack
+    {
+        private int[] stack = new int[10];
+        private int sp = 0;
+
+        public void Push(int v)
+        {
+            stack[sp++] = v;
+            if (sp >= stack.Length)
+            {
+                int newLength = stack.Length * 2;
+                int[] newStack = new int[newLength];
+                for(int i = 0; i < newLength; i++)
+                {
+                    newStack[i] = stack[0];
+                }
+                stack = newStack;
+            }
+        }
+
+        public int Pop()
+        {
+            return stack[--sp];
+        }
+
+        public int Top
+        {
+            get
+            {
+                return stack[sp - 1];
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return sp == 0;
+            }
+        }
+    }
+
+    class Arrays_Stacks
+    {
+        static void Main()
+        {
+            Stack stack = new Stack();
+
+            stack.Push(5);
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(9);
+
+            Console.WriteLine("The last value pushed was {0}", stack.Top);
+
+            while (!stack.IsEmpty)
+            {
+                Console.WriteLine("Popping {0}", stack.Pop());
+            }
+
+            Console.Write("Press Enter...");
+            Console.ReadLine();
+        }
+    }
+}
