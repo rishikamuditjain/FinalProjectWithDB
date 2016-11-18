fa24373d79a02b6f81c8b5563e0cb5aa3e3d3567
rishikamaroo <rishikamaroo@gmail.com>
2016-11-18 03:09:31 +0000

    ProjectC#01

---
+++ FinalProject-UW/Sample.cs
@@ -0,0 +1,19 @@
+﻿internal class Sample
+{
+    protected static int origRow;
+    protected static int origCol;
+
+    protected static void WriteAt(string s, int x, int y)
+    {
+        try
+        {
+            Console.SetCursorPosition(origCol + x, origRow + y);
+            Console.Write(s);
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.Clear();
+            Console.WriteLine(e.Message);
+        }
+    }
+}
