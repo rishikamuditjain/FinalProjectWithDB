6d8681b7fab11622bc4cd8a088019b687198e793
rishikamuditjain <rishikamaroo@ymail.com>
2016-11-21 02:32:37 +0000

    r

---
+++ FinalProject-UW/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FinalProject;
+using ConsoleApplication1;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FinalProject p = new FinalProject();
+
+        }
+    }
+}
